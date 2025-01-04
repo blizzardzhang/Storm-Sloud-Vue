df1810e077aa453b0560c49e9b65dd45cbfc1f48
Blizzard <parezhang@126.com>
2025-01-04 14:02:45 +0800

    feat: footer组件

---
+++ src/components/footer/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <a-layout-footer class="footer">
+    <span>
+      Copyright © 2024-{{ new Date().getFullYear() }} Storm Cloud. All Rights Reserved
+    </span>
+  </a-layout-footer>
+</template>
+<script setup lang="ts"></script>
+
+<style scoped lang="less">
+.footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 40px;
+  color: var(--color-text-2);
+  text-align: center;
+}
+</style>
